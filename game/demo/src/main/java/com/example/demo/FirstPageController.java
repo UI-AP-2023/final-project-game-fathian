dc4c306cbb50db6c35dacfd7e3ba3dc2e525e037
fatemezahrafathian <fathianfatemehzahra@gmail.com>
2023-06-29 11:26:54 +0430

    add first page

--- game/demo/src/main/java/com/example/demo/FirstPageController.java
+++ game/demo/src/main/java/com/example/demo/FirstPageController.java
@@ -3,12 +3,19 @@ package com.example.demo;
 import javafx.event.ActionEvent;
 import javafx.event.EventType;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class FirstPageController implements Initializable{
@@ -22,13 +29,23 @@ public class FirstPageController implements Initializable{
 //
 
     @FXML
-    void signupLabel(MouseEvent event) {
-
+    void signupLabel(MouseEvent event) throws IOException {
+        Parent parent= FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login.fxml")));
+        Stage stage=(Stage) ((Node)event.getSource()).getScene().getWindow();
+        Scene scene=new Scene(parent,1550,800);
+        stage.setScene(scene);
+        stage.setTitle("login");
+        stage.show();
     }
 
     @FXML
-    void loginLabel(MouseEvent event) {
-
+    void loginLabel(MouseEvent event) throws IOException {
+        Parent parent= FXMLLoader.load(Objects.requireNonNull(getClass().getResource("signup.fxml")));
+        Stage stage=(Stage) ((Node)event.getSource()).getScene().getWindow();
+        Scene scene=new Scene(parent,1550,800);
+        stage.setScene(scene);
+        stage.setTitle("signup");
+        stage.show();
     }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
